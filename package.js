e4a366bc6a908b70258a0844288bb603a6bf258b
Martin <martin@Martins-Mac-2.local>
2016-05-14 18:00:47 +0200

    registerOnStartup option

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
-  name: 'raix:push',
-  version: '3.0.2',
+  name: 'xonatec:push',
+  version: '3.0.3',
   summary: 'Isomorphic Push notifications for APN and GCM',
   git: 'https://github.com/raix/push.git'
 });
